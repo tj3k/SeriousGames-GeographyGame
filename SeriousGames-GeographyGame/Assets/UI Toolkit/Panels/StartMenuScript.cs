dfaff85d2357fb1988d8cbf79f6bd9f05943296e
tj3k <44471061+tj3k@users.noreply.github.com>
2023-04-13 19:27:08 +0100

    Added Fonts + Function to UI
    Stylised UI of Main Menu with new fonts.
    Also added quick "next scene" and "quit game" functions to buttons.
    
    Note: I made them coroutines to give time for fade-out/in animations to play.

---
+++ SeriousGames-GeographyGame/Assets/UI Toolkit/Panels/StartMenuScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI_Toolkit.Panels
+{
+    public class StartMenuScript : MonoBehaviour
+    {
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+
+        public void StartGame()
+        {
+            StartCoroutine(StartGameCo());
+        }
+
+        public void QuitGame()
+        {
+            StartCoroutine(QuitGameCo());
+        }
+    
+        // ReSharper disable Unity.PerformanceAnalysis
+        private static IEnumerator StartGameCo()
+        {
+            Debug.Log("Loading: Next Scene");
+            yield return new WaitForSeconds(2f);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        private static IEnumerator QuitGameCo()
+        {
+            Debug.Log("Quitting: Application");
+            yield return new WaitForSeconds(1f);
+            Application.Quit();
+        }
+    }
+}
